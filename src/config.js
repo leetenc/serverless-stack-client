f7ee50435b2442a3c7aa9384d8fef884cc83c5e7
leetenc <lchin@imsystems.com.au>
2020-11-04 16:23:43 +1100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-west-2",
+        BUCKET: "notes.uploads.stores01"
+    },
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "https://j7l54gk0i9.execute-api.us-west-2.amazonaws.com/prod "
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_blyZmIAbF",
+        APP_CLIENT_ID: "2tlurfc41qa6g1q1l133nrmj5v",
+        IDENTITY_POOL_ID: "us-west-2:6264953c-494f-4a9a-9935-13275e2777f8"
+    }
+};
